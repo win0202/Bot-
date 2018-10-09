424d6ac3ced9503346527a99fd7bb31369eee825
win0202 <43890623+win0202@users.noreply.github.com>
2018-10-09 22:49:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ bot.on("message", async message => {
   let prefix = '/';
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
-  let args = messageArray.slice(1);
+//  let args = messageArray.slice(1);
+let args = messageArray[1];
 
   if (cmd === `${prefix}ping`){
     message.channel.send("Pong!");
