71262e091b02ad4997c5d322ab91614859fd3bff
win0202 <43890623+win0202@users.noreply.github.com>
2018-10-09 23:29:40 +0900

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
 //  let args = messageArray.slice(1);
   let args = messageArray[1];
-  let _args = messageArray[2];
-  
-  
+ 
   if (cmd === `${prefix}ping`){
     message.channel.send("Pong!");
   }
@@ -128,8 +126,11 @@ bot.on("message", async message => {
     if (args ===`연속책략`){
        message.channel.send("장수\n번쾌 (검사)\n주유 (도독)\n곽가 (도사)\n대교 (무희)\n순욱 (책사)\n전풍 (책사)\n채염 (책사)\n방통 (현자)\n아린 (현자)\n장각 (현자)\n\n\n\n 보물\n 무귀보도 ★7 (보도)\n백학선 ★7 (선)");
     }
-    if (args === `연속` && _args === `책략`){
-      message.channel.send("장수\n번쾌 (검사)\n주유 (도독)\n곽가 (도사)\n대교 (무희)\n순욱 (책사)\n전풍 (책사)\n채염 (책사)\n방통 (현자)\n아린 (현자)\n장각 (현자)\n\n\n\n 보물\n 무귀보도 ★7 (보도)\n백학선 ★7 (선)");
+    if (args === `연속`){
+      let _args = messageArray[2];
+      if (_args === `책략`){
+         message.channel.send("장수\n번쾌 (검사)\n주유 (도독)\n곽가 (도사)\n대교 (무희)\n순욱 (책사)\n전풍 (책사)\n채염 (책사)\n방통 (현자)\n아린 (현자)\n장각 (현자)\n\n\n\n 보물\n 무귀보도 ★7 (보도)\n백학선 ★7 (선)");
+      }
     } 
 
   }
