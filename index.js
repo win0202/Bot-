a9d0da7f3564c1023f928c7880a463fab8d72369
win0202 <43890623+win0202@users.noreply.github.com>
2018-10-09 22:33:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,21 +21,40 @@ bot.on("message", async message => {
   }
 
   if (cmd === `${prefix}번역`){
-    message.channel.send("Pong!");
+    if (args ===`강보`){
+       message.channel.send("Jiang Fu");
+    }
+    if (args ===`Jiang Fu`){
+       message.channel.send("강보");
+    }
   }
   if (cmd === `${prefix}특성`){
-    message.channel.send("Pong!");
+    message.channel.send(" ");
   }
   if (cmd === `${prefix}보물`){
-    message.channel.send("Pong!");
+    message.channel.send(" ");
   }
-
+   if (cmd === `${prefix}위키`){
+    message.channel.send("https://bit.ly/2J2MXNu");
+  }
+  if (cmd === `${prefix}네이버`){
+    message.channel.send("https://cafe.naver.com/nexonjojo");
+  } if (cmd === `${prefix}플러그`){
+    message.channel.send("https://www.plug.game/OfficialCaocao_nexon");
+  }
+  if (cmd === `${prefix}코스트`){
+    message.channel.send("https://jojo.jy.is/cost/");
+  }
 
   if (cmd === `${prefix}?`){
     message.channel.send("!번역 [이름] : 아이템 or 장수 이름 번역 ex)!번역 제갈량");
     message.channel.send("!특성 [특성 이름] : 특성 검색 ex)!특성 연속 책략");
     message.channel.send("!장수 [장수 이름] : 장수 검색 ex)!장수 제갈량");
     message.channel.send("!보물 [보물 이름] : 보물 검색 ex)!보물 백학선");
+    message.channel.send("!위키 : 조조전 온라인 나무위키");
+    message.channel.send("!네이버 : 네이버 공식 카페");
+    message.channel.send("!플러그 or !plug: 글로벌서버 공식 페이지");
+    message.channel.send("!코스트 : 코스트 계산기");
   }
 
 });
